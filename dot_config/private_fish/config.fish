2eb9dc7584726b9133c0cb45a77d4ba3433a11eb
bageljr <aryanagarwal897@gmail.com>
2021-11-03 16:43:12 -0500

    Update dot_config/private_fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -9,6 +9,7 @@ set -g theme_nerd_fonts yes
 set -g theme_display_date yes
 set -g VDPAU_DRIVER radeonsi
 set -g LIBVA_DRIVER_NAME radeonsi
+set fzf_preview_dir_cmd exa --all --color=always
 alias 'vim'=micro
 alias 'ls'=exa
 alias 'grep'=rg
