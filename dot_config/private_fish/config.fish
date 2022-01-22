3ac35b446231c572f05d7ba82ed919387895d384
bageljr <aryanagarwal897@gmail.com>
2022-01-22 14:50:14 -0600

    Update dot_SpaceVim.d/init.toml Update dot_config/btop/btop.conf Update dot_config/environment.d/envars.conf Update dot_config/private_fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -68,3 +68,5 @@ set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")
 set -xU LESS_TERMCAP_se (printf "\e[0m")
 set -xU LESS_TERMCAP_us (printf "\e[01;32m")
 set -xU LESS_TERMCAP_ue (printf "\e[0m")
+
+thefuck --alias | source
