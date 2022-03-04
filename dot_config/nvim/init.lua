025d260c272bd19dd6a209b3598ba57dcf55322e
bageljr <aryanagarwal897@gmail.com>
2022-03-04 12:15:18 -0600

    Update dot_config/nvim/init.lua Update private_dot_ssh/config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,5 +1,79 @@
 vim.opt.termguicolors = true
-require("plugins")
-require("opt")
-require("dap_setup")
-require("lsp")
+require("configs/packer")
+require("configs/lsp")
+--Set highlight on search
+vim.o.hlsearch = true
+vim.o.clipboard = "unnamedplus"
+--Make line numbers default
+vim.wo.number = true
+
+--Enable mouse mode
+vim.o.mouse = "a"
+
+--Enable break indent
+vim.o.breakindent = true
+
+--Save undo history
+vim.opt.undofile = true
+vim.o.autowrite = true
+vim.o.smartindent = true
+vim.o.expandtab = true
+vim.o.shiftwidth = 3
+vim.o.tabstop = 3
+--Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+--Decrease update time
+vim.o.updatetime = 251
+vim.wo.signcolumn = "yes"
+
+--Set colorscheme
+vim.o.termguicolors = true
+vim.cmd([[colorscheme onedark]])
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = "menuone,noselect"
+
+--Remap space as leader key
+vim.api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true, silent = true })
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+--Remap for dealing with word wrap
+vim.api.nvim_set_keymap("n", "k", "v:count == 1 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
+vim.api.nvim_set_keymap("n", "j", "v:count == 1 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
+
+-- Highlight on yank
+vim.cmd([[
+  augroup YankHighlight
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+  augroup end
+]])
+
+--Map blankline
+vim.g.indent_blankline_char = "┊"
+vim.g.indent_blankline_filetype_exclude = { "help", "packer", "dashboard" }
+vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
+vim.g.indent_blankline_show_trailing_blankline_indent = false
+
+-- local sidebar = require("sidebar-nvim")
+-- local opts = {
+--   open = true,
+--   side = "right",
+--   sections = {
+--         "git",
+--         "diagnostics",
+--         "symbols",
+--         require("dap-sidebar-nvim.breakpoints")
+--   },
+--   dap = {
+--         breakpoints = {
+--             icon = "🔍"
+--         }
+--   },
+--   disable_closing_prompt = true
+-- }
+-- sidebar.setup(opts)
+vim.api.nvim_set_keymap("n", "q", [[<cmd> q <CR>]], { noremap = true, silent = true })
