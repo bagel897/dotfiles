94a5e85bceb64303b3028229b6d8c30c67b640b9
bageljr <aryanagarwal897@gmail.com>
2022-03-15 18:51:52 -0500

    Update dot_config/private_fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -83,6 +83,6 @@ set -xU LESS_TERMCAP_se (printf "\e[0m")
 set -xU LESS_TERMCAP_us (printf "\e[01;32m")
 set -xU LESS_TERMCAP_ue (printf "\e[0m")
 
-status is-login; and pyenv init --path | source
-status is-interactive; and pyenv init - | source
+# status is-login; and pyenv init --path | source
+# status is-interactive; and pyenv init - | source
 set -g theme_display_git no
