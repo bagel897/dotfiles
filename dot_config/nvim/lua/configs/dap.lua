d7ae19243166c21bb35137c5e2fe803a01ad1196
bageljr <bageljr897@protonmail.com>
2022-04-03 18:07:35 -0500

    Update dot_config/nvim/lua/configs/dap.lua

--- dot_config/nvim/lua/configs/dap.lua
+++ dot_config/nvim/lua/configs/dap.lua
@@ -39,79 +39,27 @@ dap.adapters.lldb = {
 	type = "executable",
 	command = "/usr/bin/lldb-vscode", -- adjust as needed
 	name = "lldb",
-			env = {
-			LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = "YES",
-		},
 }
-dap.adapters.codelldb = function(on_adapter)
-	local stdout = vim.loop.new_pipe(false)
-	local stderr = vim.loop.new_pipe(false)
-
-	-- CHANGE THIS!
-	local cmd = "/home/bageljr/Downloads/extension/adapter/codelldb"
-
-	local handle, pid_or_err
-	local opts = {
-		stdio = { nil, stdout, stderr },
-		detached = true,
-	}
-	handle, pid_or_err = vim.loop.spawn(cmd, opts, function(code)
-		stdout:close()
-		stderr:close()
-		handle:close()
-		if code ~= 0 then
-			print("codelldb exited with code", code)
-		end
-	end)
-	assert(handle, "Error running codelldb: " .. tostring(pid_or_err))
-	stdout:read_start(function(err, chunk)
-		assert(not err, err)
-		if chunk then
-			local port = chunk:match("Listening on port (%d+)")
-			if port then
-				vim.schedule(function()
-					on_adapter({
-						type = "server",
-						host = "127.0.0.1",
-						port = port,
-					})
-				end)
-			else
-				vim.schedule(function()
-					require("dap.repl").append(chunk)
-				end)
-			end
-		end
-	end)
-	stderr:read_start(function(err, chunk)
-		assert(not err, err)
-		if chunk then
-			vim.schedule(function()
-				require("dap.repl").append(chunk)
-			end)
-		end
-	end)
-end
 dap.configurations.cpp = {
 	{
-		name = "test file lldb",
+		name = "test1",
 		type = "lldb",
-		program = "${workspaceFolder}/cpp.out",
 		request = "launch",
+		program = "${workspaceFolder}/cpp.out",
 		cwd = "${workspaceFolder}",
-		stdio = { "${workspaceFolder}/input1", nil, nil },
-		args = {},
+		args = { "<input1" },
+		runInTerminal = true,
+		postRunCommands = {'process handle -p true -s false -n false SIGWINCH'}
 	},
 	{
-		name = "Launch via codelldb",
+		name = "test2",
 		type = "lldb",
 		request = "launch",
-		program = function()
-			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-		end,
-		stdio = { "input.txt", nil, nil },
+		program = "${workspaceFolder}/cpp.out",
 		cwd = "${workspaceFolder}",
-		args = {},
+		args = { "<input2" },
+		runInTerminal = true,
+		postRunCommands = {'process handle -p true -s false -n false SIGWINCH'}
 	},
 	{
 		name = "Launch",
