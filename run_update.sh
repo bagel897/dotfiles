b065088b1a9c48cd1ca641308dd6a71df158db4b
bageljr <bageljr897@protonmail.com>
2022-04-13 10:03:01 -0500

    Update run_update.sh

--- run_update.sh
+++ run_update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerCompile'
 if type -p fish > /dev/null; then
 	fish -c "fisher update > /dev/null"
 fi
