40f26f63cc58ff43518b9fdb1572fdda1812145b
bageljr <aryanagarwal897@gmail.com>
2022-04-18 19:58:03 -0500

    add chezmoi

--- nix_install.sh
+++ nix_install.sh
@@ -5,4 +5,5 @@ nix-env -iA nixpkgs.python310Packages.python-lsp-server
 nix-env -iA nixpkgs.python310Packages.pylsp-mypy
 nix-env -iA nixpkgs.python310Packages.python-lsp-black
 nix-env -iA nixpkgs.python310Packages.pyls-isort
+nix-env -iA nixos.chezmoi
 fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
