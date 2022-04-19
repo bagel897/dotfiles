eebd1acb673b86a3b8d1751ee05a4999d2660daf
bageljr <aryanagarwal897@gmail.com>
2022-04-18 20:05:02 -0500

    Update dot_config/private_fish/fish_plugins Update nix_install.sh

--- nix_install.sh
+++ nix_install.sh
@@ -1,10 +1,8 @@
 . ~/.nix-profile/etc/profile.d/nix.sh
-nix-env -iA nixpkgs.neovim nixpkgs.fd nixpkgs.exa nixpkgs.fzf nixpkgs.ripgrep nixpkgs.delta nixpkgs.fish nixpkgs.git
+nix-env -iA nixpkgs.neovim nixpkgs.fd nixpkgs.exa nixpkgs.fzf nixpkgs.ripgrep nixpkgs.delta nixpkgs.fish 
 nix-env -iA nixpkgs.nodePackages.bash-language-server
 nix-env -iA nixpkgs.python310Packages.python-lsp-server
 nix-env -iA nixpkgs.python310Packages.pylsp-mypy
 nix-env -iA nixpkgs.python310Packages.python-lsp-black
 nix-env -iA nixpkgs.python310Packages.pyls-isort
-nix-env -iA nixpkgs.chezmoi
-nix-env -iA nixpkgs.bat
 fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
