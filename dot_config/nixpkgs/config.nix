7e0262955110603e4132ef86bc2c8fd093de2c62
bageljr <bageljr897@protonmail.com>
2022-04-24 13:38:08 -0500

    Add dot_config/nixpkgs/config.nix Update nix_install.sh

---
+++ dot_config/nixpkgs/config.nix
@@ -0,0 +1,28 @@
+{
+  packageOverrides = pkgs: with pkgs; {
+    myPackages = pkgs.buildEnv {
+      name = "my-packages";
+      paths = [
+		pkgs.neovim
+		pkgs.fd
+		pkgs.exa
+		pkgs.fzf
+		pkgs.ripgrep
+		pkgs.delta
+		pkgs.fish
+		pkgs.nodePackages.bash-language-server
+		pkgs.python310Packages.python-lsp-server
+		pkgs.mypy
+		pkgs.git
+		pkgs.fish
+		# pkgs.pip
+		pkgs.chezmoi
+		pkgs.poetry
+		# pkgs.python310Packages.pylsp-mypy
+		pkgs.python310Packages.python-lsp-black
+                pkgs.python310Packages.pyls-isort
+      ];
+    };
+  };
+}
+
--- nix_install.sh
+++ nix_install.sh
@@ -1,8 +1,3 @@
 . ~/.nix-profile/etc/profile.d/nix.sh
-nix-env -iA nixpkgs.neovim nixpkgs.fd nixpkgs.exa nixpkgs.fzf nixpkgs.ripgrep nixpkgs.delta nixpkgs.fish 
-nix-env -iA nixpkgs.nodePackages.bash-language-server
-nix-env -iA nixpkgs.python310Packages.python-lsp-server
-nix-env -iA nixpkgs.python310Packages.pylsp-mypy
-nix-env -iA nixpkgs.python310Packages.python-lsp-black
-nix-env -iA nixpkgs.python310Packages.pyls-isort
+nix-env -iA nixpkgs.myPackages
 fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
