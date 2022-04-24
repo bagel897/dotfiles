f8034150262c712e6ddd229759454c5ccc8d9c85
bageljr <bageljr897@protonmail.com>
2022-04-24 15:06:08 -0500

    use custom package

--- nix_install.sh
+++ nix_install.sh
@@ -1,3 +1,3 @@
 . ~/.nix-profile/etc/profile.d/nix.sh
-nix-env -iA nixpkgs.myPackage
+nix-env -iA nixpkgs.myPackages
 fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
