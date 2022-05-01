90f4eec4cb33d4f7bf5ee4ee332997bcb8d75668
bageljr <bageljr897@protonmail.com>
2022-05-01 16:15:15 -0500

    Update dot_config/nvim/lua/configs/dap.lua Update dot_config/nvim/lua/configs/packer.lua Update dot_config/private_fish/fish_plugins

--- dot_config/nvim/lua/configs/packer.lua
+++ dot_config/nvim/lua/configs/packer.lua
@@ -274,7 +274,7 @@ return require("packer").startup({
 			"p00f/cphelper.nvim",
 			config = function()
 				vim.g.cphlang = "python"
-				vim.g.cpp_compile_command = "g++ -g solution.cpp -o cpp.out"
+				vim.g.cpp_compile_command = "g++ -g solution.cpp -o solution"
 			end,
 		})
 		use({
