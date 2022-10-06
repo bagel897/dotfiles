176e7f09d3400bc238f39dc01fcd504ca9587c38
bagel897 <bageljr897@protonmail.com>
2022-10-06 13:35:06 -0500

    Update dot_config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -4,5 +4,22 @@ return {
 	font = wezterm.font("FiraCode Nerd Font Mono"),
 
 	color_scheme = "tokyonight-storm",
+	ssh_domains = {
+		{
+			name = "rocket",
+			remote_address = "10.157.95.31",
+			username = "aa87445",
+			assume_shell = "Posix",
+			remote_wezterm_path = "/home/aa87445/.local/bin/wezterm",
+		},
+		{
+			name = "cs",
+			remote_address = "trix.cs.utexas.edu",
+			username = "bageljr",
+			assume_shell = "Posix",
+			remote_wezterm_path = "/u/bageljr/.local/bin/wezterm",
+		},
+	},
+
 	-- window_background_opacity = 0.9,
 }
