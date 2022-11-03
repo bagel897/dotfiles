0abad3be696870368722e9cec98a4e49664ad92d
bagel897 <bageljr897@protonmail.com>
2022-11-02 21:44:18 -0500

    Update .config/nvim/init.lua Update .config/nvim/lua/configs/cmp.lua Add .config/nvim/lua/configs/lsp.lua Remove .config/nvim/lua/configs/lsp.lua Update .config/nvim/lua/configs/packer.lua

--- dot_config/nvim/lua/configs/packer.lua
+++ dot_config/nvim/lua/configs/packer.lua
@@ -26,6 +26,7 @@ packer.init({
 	auto_clean = true,
 	compile_on_sync = true,
 })
+
 return require("packer").startup({
 	function(use)
 		use({
@@ -152,7 +153,7 @@ return require("packer").startup({
 				}
 			end,
 		})
-		use({ "wbthomason/packer.nvim", opt = false })
+		use({ "wbthomason/packer.nvim" })
 		use({
 			"TimUntersberger/neogit",
 			requires = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim" },
@@ -324,6 +325,22 @@ return require("packer").startup({
 			end,
 		})
 		use({
+
+			"petertriho/cmp-git",
+			config = function()
+				require("cmp_git").setup()
+			end,
+		})
+		use({
+			"L3MON4D3/LuaSnip",
+			"rafamadriz/friendly-snippets",
+			config = function()
+				require("luasnip.loaders.from_vscode").lazy_load()
+				require("luasnip.loaders.from_snipmate").lazy_load()
+			end
+		})
+
+		use({
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
@@ -331,18 +348,11 @@ return require("packer").startup({
 			"hrsh7th/cmp-nvim-lsp-document-symbol",
 			"hrsh7th/nvim-cmp",
 			"rcarriga/cmp-dap",
-			"L3MON4D3/LuaSnip",
 			"davidsierradz/cmp-conventionalcommits",
 			"saadparwaiz1/cmp_luasnip",
 			"onsails/lspkind-nvim",
-			"petertriho/cmp-git",
-			"rafamadriz/friendly-snippets",
 			"ray-x/cmp-treesitter",
 			requires = { "nvim-lua/plenary.nvim" },
-			after = { "nvim-lspconfig" },
-			config = function()
-				require("configs/cmp").setup()
-			end,
 		})
 		use({
 			"williamboman/mason.nvim",
@@ -350,6 +360,7 @@ return require("packer").startup({
 				require("mason").setup({})
 			end,
 		})
+
 		use({
 			"williamboman/mason-lspconfig.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
@@ -392,11 +403,209 @@ return require("packer").startup({
 			end,
 		})
 		use({
-			"neovim/nvim-lspconfig",
-			"jose-elias-alvarez/null-ls.nvim",
 			"p00f/clangd_extensions.nvim",
 			config = function()
-				require("configs/lsp").setup()
+
+				require("clangd_extensions").setup({
+					server = {
+						cmd = {
+							"clangd",
+							"--background-index",
+							"-j=12",
+							"--query-driver=/usr/bin/**/clang-*,/bin/clang,/bin/clang++,/usr/bin/gcc,/usr/bin/g++",
+							"--clang-tidy",
+							"--all-scopes-completion",
+							"--cross-file-rename",
+							"--completion-style=detailed",
+							"--header-insertion-decorators",
+							"--header-insertion=iwyu",
+							"--pch-storage=memory",
+						},
+						capabilities = require("cmp_nvim_lsp").default_capabilities(),
+						on_attach = require("lsp-format").on_attach,
+					},
+				})
+
+			end
+
+		})
+		use({
+			"jose-elias-alvarez/null-ls.nvim",
+			config = function()
+				local null_ls = require("null-ls")
+				local sources = {
+					-- null_ls.builtins.formatting.clang_format,
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.diagnostics.rstcheck,
+					null_ls.builtins.formatting.prettier,
+					-- null_ls.builtins.formatting.joker,
+					-- null_ls.builtins.diagnostics.clj_kondo,
+					null_ls.builtins.formatting.cljstyle,
+					-- null_ls.builtins.code_actions.gitsigns,
+					-- null_ls.builtins.diagnostics.gitlint,
+					-- null_ls.builtins.diagnostics.luacheck,
+					-- null_ls.builtins.diagnostics.pylint,
+					-- null_ls.builtins.code_actions.refactoring,
+					-- null_ls.builtins.formatting.black,
+					-- Black can be finnicky on the lsp side so I use Null to back it up
+				}
+				require("null-ls").setup({
+					sources = sources,
+					on_attach = require("lsp-format").on_attach,
+				})
+			end
+		})
+		use({
+			"neovim/nvim-lspconfig",
+			config = function()
+				-- Mappings.
+				-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+				-- vim.g.coq_settings = { auto_start = "shut-up" }
+				--
+				local capabilities = require("cmp_nvim_lsp").default_capabilities()
+				local on_attach = function(client, bufnr)
+					require("lsp-format").on_attach(client)
+				end
+				-- Use a loop to conveniently call 'setup' on multiple servers and
+				-- map buffer local keybindings when the language server attaches
+				vim.g.markdown_fenced_languages = {
+					"ts=typescript",
+				}
+				local servers = {
+					"taplo",
+					"clojure_lsp",
+					-- "texlab",
+					"ltex",
+					"rust_analyzer",
+					-- "prosemd_lsp",
+					-- "ccls",
+					-- "clangd",
+					"vimls",
+					"bashls",
+					-- "sumneko_lua",
+					"jsonls",
+					"cmake",
+					"esbonio",
+					"dockerls",
+					-- "pylsp",
+					-- "pyright",
+					-- "jedi_language_server",
+					"gdscript",
+				}
+				require("lspconfig").denols.setup({
+					init_options = {
+						lint = true,
+					},
+					capabilities = capabilities,
+					on_attach = on_attach,
+				})
+				-- local coq = require("coq")
+				for _, lsp in pairs(servers) do
+					require("lspconfig")[lsp].setup({
+						capabilities = capabilities,
+						-- flags = {
+						-- 	debounce_text_changes = 150,
+						-- },
+						on_attach = on_attach,
+					})
+				end
+				-- require("lspconfig").sourcery.setup({
+				-- 	capabilities = capabilities,
+				-- 	on_attach = on_attach,
+				-- 	init_options = {
+				-- 		editor_version = "vim",
+				-- 		extension_version = "vim.lsp",
+				--
+				-- 	},
+				-- })
+				require("lspconfig").pylsp.setup({
+					settings = {
+						pylsp = {
+							plugins = {
+								pycodestyle = { enabled = true, maxLineLength = 88 },
+								-- pydocstyle = { enabled = true },
+								rope_autoimport = { enabled = true, memory = false },
+								pylsp_mypy = {
+									enabled = true,
+									live_mode = false,
+									dmypy = true,
+								},
+								-- pylint = { enabled = true, args = {"--disable C0301"}},
+							},
+						},
+					},
+					capabilities = capabilities,
+					on_attach = on_attach,
+				})
+				require("lspconfig").texlab.setup({
+					settings = {
+						texlab = {
+							auxDirectory = ".",
+							bibtexFormatter = "texlab",
+							build = {
+								args = { "--keep-logs", "--keep-intermediates", "-synctex", "$f" },
+								executable = "tectonic",
+								forwardSearchAfter = false,
+								onSave = true,
+							},
+							chktex = {
+								onEdit = false,
+								onOpenAndSave = true,
+							},
+							diagnosticsDelay = 300,
+							formatterLineLength = 80,
+							forwardSearch = {
+								args = {},
+							},
+							latexFormatter = "latexindent",
+							latexindent = {
+								modifyLineBreaks = true,
+							},
+						},
+					},
+					capabilities = capabilities,
+					on_attach = on_attach,
+				})
+				require("lspconfig").sumneko_lua.setup({
+					settings = {
+						completion_style = {
+							call_snipped = "Replace",
+						},
+						Lua = {
+							hint = {
+								enable = true,
+							},
+							runtime = {
+								-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+								version = "LuaJIT",
+								-- Setup your lua path
+							},
+							diagnostics = {
+								-- Get the language server to recognize the `vim` global
+								globals = { "vim" },
+							},
+							workspace = {
+								-- Make the server aware of Neovim runtime files
+								library = vim.api.nvim_get_runtime_file("", true),
+							},
+							-- Do not send telemetry data containing a randomized but unique identifier
+							telemetry = {
+								enable = false,
+							},
+						},
+					},
+					capabilities = capabilities,
+					on_attach = on_attach,
+				})
+
+
+				vim.diagnostic.config({
+					virtual_text = false,
+					signs = true,
+					underline = true,
+					update_in_insert = false,
+					severity_sort = true,
+				})
 			end,
 		})
 		use({
@@ -540,6 +749,7 @@ return require("packer").startup({
 				require("mason-null-ls").setup({
 					automatic_installation = true,
 					auto_update = true,
+					automatic_setup = true,
 				})
 			end,
 		})
@@ -562,6 +772,49 @@ return require("packer").startup({
 				{ "nvim-telescope/telescope.nvim" },
 			},
 			config = function()
+				local cfg = require("yaml-companion").setup({
+					{
+						-- Built in file matchers
+						builtin_matchers = {
+							-- Detects Kubernetes files based on content
+							kubernetes = { enabled = true },
+						},
+
+						-- Additional schemas available in Telescope picker
+						schemas = {
+							result = {
+								--{
+								--  name = "Kubernetes 1.22.4",
+								--  uri = "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.22.4-standalone-strict/all.json",
+								--},
+							},
+						},
+
+						-- Pass any additional options that will be merged in the final LSP config
+						lspconfig = {
+							flags = {
+								debounce_text_changes = 150,
+							},
+							settings = {
+								redhat = { telemetry = { enabled = false } },
+								yaml = {
+									validate = true,
+									format = { enable = true },
+									hover = true,
+									schemaStore = {
+										enable = true,
+										url = "https://www.schemastore.org/api/json/catalog.json",
+									},
+									schemaDownload = { enable = true },
+									schemas = {},
+									trace = { server = "debug" },
+								},
+							},
+							on_attach = require("lsp-format").on_attach,
+						},
+					},
+				})
+				require("lspconfig")["yamlls"].setup(cfg)
 				require("telescope").load_extension("yaml_schema")
 			end,
 		})
@@ -598,10 +851,6 @@ return require("packer").startup({
 		})
 		use({
 			"folke/noice.nvim",
-			event = "VimEnter",
-			config = function()
-				require("noice").setup()
-			end,
 			requires = {
 				-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 				"MunifTanjim/nui.nvim",
@@ -610,7 +859,6 @@ return require("packer").startup({
 				--   If not available, we use `mini` as the fallback
 				"rcarriga/nvim-notify",
 			},
-			after = "nvim-cmp",
 		})
 		use({
 			"s1n7ax/nvim-window-picker",
