1551f006aa317e3f84398a2f763c538dbe8392ce
bagel897 <bageljr897@protonmail.com>
2022-12-30 14:04:01 -0600

    Remove .config/nvim/lua/configs/lsp.lua Remove .config/nvim/lua/configs/legendary.lua Remove .config/nvim/lua/configs/neo-tree.lua Remove .config/nvim/lua/configs/splits.lua Remove .config/nvim/lua/configs/telescope.lua Remove .config/nvim/lua/configs/tree.lua Remove .config/nvim/lua/configs/treesitter.lua Remove .config/nvim/lua/configs/trouble.lua Remove update.sh

--- run_update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash	
-if type -P nix > /dev/null; then
-	bash ~/.local/share/chezmoi/nix_update.sh
-else if type -P nix-user-chroot > /dev/null; then
-      nix-user-chroot ~/.nix bash ~/.local/share/chezmoi/nix_update.sh
-fi fi
