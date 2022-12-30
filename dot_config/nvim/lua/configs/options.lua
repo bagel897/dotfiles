138ee3d0477e807c1d0b0f496936b450039c4d3a
bagel897 <bageljr897@protonmail.com>
2022-12-30 16:21:43 -0600

    Add .config/nvim/lua/configs/lazy.lua Add .config/nvim/lua/configs/options.lua

---
+++ dot_config/nvim/lua/configs/options.lua
@@ -0,0 +1,51 @@
+vim.opt.termguicolors = true
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+--Set highlight on search
+vim.o.hlsearch = true
+vim.o.clipboard = "unnamedplus"
+vim.o.guifont = "FiraCode NF"
+vim.g.adwaita_darker = true
+vim.cmd("set spell")
+--Make line numbers default
+vim.wo.number = true
+
+--Enable mouse mode
+vim.o.mouse = "a"
+
+--Enable break indent
+-- vim.o.breakindent = true
+-- vim.o.tabstop = 4
+-- vim.o.shiftwidth = 4
+-- vim.o.expandtab = true
+-- vim.o.cindent = true
+--Save undo history
+vim.opt.undofile = true
+vim.o.autowriteall = true
+--Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+--Decrease update time
+vim.o.updatetime = 251
+vim.wo.signcolumn = "yes"
+
+vim.o.autoread = true
+vim.o.splitkeep = "screen"
+--Remap space as leader key
+--Remap for dealing with word wrap
+vim.api.nvim_set_keymap("n", "k", "v:count == 1 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
+vim.api.nvim_set_keymap("n", "j", "v:count == 1 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
+vim.keymap.set("n", "q", [[<cmd>q<CR>]])
+vim.cmd([[autocmd FocusGained,BufEnter,CursorHold,CursorHoldI * if mode() != 'c' | checktime | endif]])
+vim.cmd(
+	[[autocmd FileChangedShellPost *\ echohl WarningMsg | lua vim.notify("File changed on disk. Buffer reloaded.") | echohl None]]
+)
+vim.o.completeopt = "menu,menuone,noselect"
+vim.diagnostic.config({
+	virtual_text = false,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
+})
