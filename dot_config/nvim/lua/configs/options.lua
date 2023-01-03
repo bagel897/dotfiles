843a3ac489039044f58001c8ba4a70914bfdee8c
bagel897 <bageljr897@protonmail.com>
2023-01-03 12:15:22 -0600

    Update .config/nvim/lua/configs/options.lua Update .config/nvim/lua/configs/plugins/rust-tools.lua

--- dot_config/nvim/lua/configs/options.lua
+++ dot_config/nvim/lua/configs/options.lua
@@ -48,6 +48,6 @@ vim.diagnostic.config({
 	virtual_text = false,
 	signs = true,
 	underline = true,
-	update_in_insert = false,
+	update_in_insert = true,
 	severity_sort = true,
 })
--- dot_config/nvim/lua/configs/plugins/rust-tools.lua
+++ dot_config/nvim/lua/configs/plugins/rust-tools.lua
@@ -1,9 +1,12 @@
 return {
 	"simrat39/rust-tools.nvim",
 	config = function()
+		local extension_path = vim.env.HOME .. "/.local/share/nvim/mason/packages/codelldb/extension/"
+		local codelldb_path = extension_path .. "adapter/codelldb"
+		local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
 		local opts = {
 			dap = {
-				adapter = require("rust-tools.dap").get_codelldb_adapter("/usr/bin/codelldb", "/usr/lib/libldb.so"),
+				adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
 			},
 		}
 
