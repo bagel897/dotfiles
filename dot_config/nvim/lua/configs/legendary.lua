a35b2a23589e8e4a53713c29bfeaa9027e62ce09
bagel897 <bageljr897@protonmail.com>
2023-02-02 18:23:15 -0600

    remove old files

--- dot_config/nvim/lua/configs/legendary.lua
+++
@@ -1,9 +0,0 @@
-require("legendary").setup({
-	autocmds = {
-		{ name = "YankHighlight", { "TextYankPost ", vim.highlight.on_yank } },
-		{
-			name = "packer_user_config",
-			{ "BufWritePost", "source <afile> | PackerCompile", opts = { pattern = "*/configs/packer.lua" } },
-		},
-	},
-})
--- dot_config/nvim/lua/configs/trouble.lua
+++
@@ -1,8 +0,0 @@
-local trouble = require("trouble")
-trouble.setup({
-	auto_open = true,
-	auto_close = true,
-	-- auto_fold = true,
-	use_diagnostic_signs = true,
-	mode = "document_diagnostics",
-})
