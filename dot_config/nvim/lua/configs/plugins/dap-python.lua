1a615737d3ca8d23b298ac65f06dedc1a9efb263
bagel897 <bageljr897@protonmail.com>
2023-02-13 13:34:24 -0600

    Update .config/nvim/lua/configs/plugins/dap-python.lua

--- dot_config/nvim/lua/configs/plugins/dap-python.lua
+++ dot_config/nvim/lua/configs/plugins/dap-python.lua
@@ -1,17 +1,17 @@
 return {
-	"mfussenegger/nvim-dap-python",
-	config = function()
-		require("dap-python").setup("python")
-		require('dap-python').test_runner = 'pytest'
-	end,
-	keys = {
-		{
-			"<leader>dds",
-			function()
-				require("dap-python").debug_selection()
-			end,
-			desc = "debug selection python",
-		},
-	},
-	ft = "python",
+    "mfussenegger/nvim-dap-python",
+    config = function()
+	    require("dap-python").setup("python")
+	    require('dap-python').test_runner = 'pytest'
+    end,
+    keys = {
+        {
+            "<leader>dds",
+            function()
+	            require("dap-python").debug_selection()
+            end,
+            desc = "debug selection python",
+        },
+    },
+    ft = "python",
 }
