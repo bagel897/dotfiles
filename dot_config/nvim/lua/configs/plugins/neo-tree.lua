9b7f32b573322bc376888b128f088263a4ed79f8
bagel897 <bageljr897@protonmail.com>
2023-03-20 14:59:46 -0500

    Update .config/nvim/lua/configs/plugins/neo-tree.lua

--- dot_config/nvim/lua/configs/plugins/neo-tree.lua
+++ dot_config/nvim/lua/configs/plugins/neo-tree.lua
@@ -34,7 +34,8 @@ local opts = {
 		diag_sort_function = "severity", -- "severity" means diagnostic items are sorted by severity in addition to their positions.
 		-- "position" means diagnostic items are sorted strictly by their positions.
 		-- May also be a function.
-		follow_behavior = { -- Behavior when `follow_current_file` is true
+		follow_behavior = {
+			-- Behavior when `follow_current_file` is true
 			always_focus_file = false, -- Focus the followed file, even when focus is currently on a diagnostic item belonging to that file.
 			expand_followed = true, -- Ensure the node of the followed file is expanded
 			collapse_others = true, -- Ensure other nodes are collapsed
@@ -46,7 +47,6 @@ local opts = {
 	},
 }
 return {
-
 	"nvim-neo-tree/neo-tree.nvim",
 	keys = {
 		{ "<C-n>", "<cmd> :Neotree toggle<cr>", desc = "Open Neo-tree" },
@@ -56,6 +56,27 @@ return {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
+		{
+			-- only needed if you want to use the commands with "_with_window_picker" suffix
+			"s1n7ax/nvim-window-picker",
+			tag = "v1.*",
+			config = function()
+				require("window-picker").setup({
+					autoselect_one = true,
+					include_current = false,
+					filter_rules = {
+						-- filter using buffer options
+						bo = {
+							-- if the file type is one of following, the window will be ignored
+							filetype = { "neo-tree", "neo-tree-popup", "notify" },
+							-- if the buffer type is one of following, the window will be ignored
+							buftype = { "terminal", "quickfix" },
+						},
+					},
+					other_win_hl_color = "#e35e4f",
+				})
+			end,
+		},
 	},
 	config = function()
 		require("neo-tree").setup(opts)
