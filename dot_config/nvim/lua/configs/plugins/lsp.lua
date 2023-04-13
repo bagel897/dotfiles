5299299638a83e6db6fbd3457a50a1f5e319cc44
bagel897 <bageljr897@protonmail.com>
2023-04-12 20:05:13 -0500

    Update .config/nvim/lua/configs/plugins/lsp.lua Update .config/nvim/lua/configs/plugins/rust-tools.lua

--- dot_config/nvim/lua/configs/plugins/lsp.lua
+++ dot_config/nvim/lua/configs/plugins/lsp.lua
@@ -108,6 +108,7 @@ return {
 		"folke/neodev.nvim",
 		"mason-lspconfig.nvim",
 		"lsp-inlayhints.nvim",
+		"lsp_lines.nvim",
 		{
 			"b0o/SchemaStore.nvim",
 			version = false, -- last release is way too old
--- dot_config/nvim/lua/configs/plugins/rust-tools.lua
+++ dot_config/nvim/lua/configs/plugins/rust-tools.lua
@@ -4,14 +4,11 @@ return {
 		local extension_path = vim.env.HOME .. "/.local/share/nvim/mason/packages/codelldb/extension/"
 		local codelldb_path = extension_path .. "adapter/codelldb"
 		local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
-		local on_attach = require("configs.on_attach")
-		local on_attach_ih = function(c, b)
-			on_attach(c, b, true)
-		end
+		local on_attach_ih = require("configs.on_attach_ih")
 		local opts = {
 			tools = {
 				inlay_hints = {
-					auto = false,
+					auto = true,
 				},
 			},
 			dap = {
