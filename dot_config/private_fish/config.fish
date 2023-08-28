b001350568ea63661a9fef7779dbbceba65c6b82
bagel897 <ellenagarwal897@gmail.com>
2023-08-28 13:12:46 -0500

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -31,10 +31,11 @@ alias 'man'=batman
 alias diff='diff --color=auto'
 alias ip='ip -color=auto'
 #alias 'cd'=z
+set PYTHON_VERSION "python$(python3 --version | sed 's/\.[0-9]*$//' | sed 's/Python //')"
 if test -n "$PYTHONPATH"
-    set -x PYTHONPATH '/home/bagel897/.local/pipx/venvs/pdm/lib/python3.10/site-packages/pdm/pep582' $PYTHONPATH
+    set -x PYTHONPATH "$HOME/.local/pipx/venvs/pdm/lib/$PYTHON_VERSION/site-packages/pdm/pep582" $PYTHONPATH
 else
-    set -x PYTHONPATH '/home/bagel897/.local/pipx/venvs/pdm/lib/python3.10/site-packages/pdm/pep582'
+    set -x PYTHONPATH "$HOME/.local/pipx/venvs/pdm/lib/$PYTHON_VERSION/site-packages/pdm/pep582"
 end
 function java-gui
     java --module-path /usr/lib/jvm/java-11-openjfx/lib --add-modules javafx.controls,javafx.fxml,javafx.web $argv
