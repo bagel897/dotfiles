8bc6051a2439234f8eac3e14da44f90715f5ce53
bagel897 <ellenagarwal897@gmail.com>
2023-08-30 10:38:38 -0500

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/config/keymaps.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "q", [[<cmd>q<CR>]])
+vim.keymap.set("x", "p", "P")
