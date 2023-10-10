666141bcc91cac66b8e4528978054c861c9c7580
bagel897 <ellenagarwal897@gmail.com>
2023-10-10 13:10:27 -0500

    Add .config/fish/conf.d/cargo.fish Update .ssh/config Add tide.sh

---
+++ run_onchange_tide.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env fish
+tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='12-hour format' --rainbow_prompt_separators=Round --powerline_prompt_heads=Round --powerline_prompt_tails=Round --powerline_prompt_height='Two lines' --prompt_connection=Solid --powerline_prompt_frame=Full --prompt_connection_andor_frame_color=Lightest --prompt_spacing=Compact --icons='Many icons'
