e0e7d53af727943bd31cb79db362679d3cd59821
bagel897 <ellenagarwal897@gmail.com>
2024-02-14 09:54:35 -0600

    Update .config/nvim/lua/config/lazy.lua

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -17,7 +17,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.dap.nlua" },
     { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.lang.rust" },
+    -- { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
