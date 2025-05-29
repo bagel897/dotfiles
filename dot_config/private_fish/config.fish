ddea65552a36bd6b2e2c4bfbb9fb3ea14dd50aae
bagel897 <ellenagarwal897@gmail.com>
2025-05-29 09:56:27 -0700

    Update .config/fish/config.fish Update .config/topgrade.toml

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -40,3 +40,10 @@ function windows
 end
 # eval "$(pdm --pep582)"
 set -e WAYLAND_DISPLAY
+
+# pnpm
+set -gx PNPM_HOME "/home/bagel897/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
